cd9d2c99f84791b4004057e5c79e5ef3c667c1d0
Broderick Westrope <broderickwestrope@gmail.com>
2024-03-01 08:52:15 +1100

    feat: setup languages
    Enable the defaults for YAML, Rust, JSON, Markdown, Docker, Tailwind, and Go.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,10 +10,16 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
